8f6373f2976c071d1d00dce050993fd47e44df45
Judith Rea Nogales <judithrea98@gmail.com>
2018-08-21 18:11:31 +0200

    Improved accessibility of site

--- src/App.js
+++ src/App.js
@@ -5,22 +5,27 @@ import ListView from './ListView'
 
 
 class App extends Component {
- constructor(props) {
-  super(props);
-  this.state = {
-    allLocations: [
-      {title: 'Fushimi Inari-taisha', position: {lat: 34.966822, lng: 135.772721}},
-      {title: 'Kiyomizu-dera', position: {lat: 34.994825, lng: 135.784909}},
-      {title: 'Kōzan-ji', position: {lat: 35.060552, lng: 135.678453}},
-      {title: 'Mount Kurama', position: {lat: 35.123135, lng: 135.776261}},
-      {title: 'Ponto-chō', position: {lat: 35.005107, lng: 135.770954}}
-    ],
-    markers: [],
-    prevMarker: null,
-    data: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      allLocations: [
+        {title: 'Fushimi Inari-taisha', position: {lat: 34.966822, lng: 135.772721}},
+        {title: 'Kiyomizu-dera', position: {lat: 34.994825, lng: 135.784909}},
+        {title: 'Kōzan-ji', position: {lat: 35.060552, lng: 135.678453}},
+        {title: 'Mount Kurama', position: {lat: 35.123135, lng: 135.776261}},
+        {title: 'Ponto-chō', position: {lat: 35.005107, lng: 135.770954}}
+      ],
+      markers: [],
+      prevMarker: null,
+      data: []
+    }
   }
 
- }
+  /*
+  * Gets the data from Wikipedia through MediaWiki API 
+  * and stores it in the 'data' variable on state. 
+  * Errors with the requested data are handled too
+  */
 
   getData(title) {
     fetch(`https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${title.replace(/\s/g, '_')}&exsentences=1&exlimit=1&origin=*`).then(r => r.json())
@@ -29,32 +34,55 @@ class App extends Component {
       let extract = content.extract;
       let parser = new DOMParser();
       let html = parser.parseFromString(extract, 'text/html');
-      let text = {title: title,
-        text: html.querySelector('body p').innerHTML};
+      let text = {
+        title: title,
+        text: html.querySelector('body p').innerHTML
+      };
+      
       this.setState(prevState => ({data: [...prevState.data, text]}));
-      // console.log(this.state.data);
-      // console.log(text);
+    }).catch((error) => {
+      console.log('Request error', error);
+      window.alert('Sorry! Data could not be loaded.');
     })
   }
+
+  /*
+  * Handles a click event for a marker or a list view item (or keypress  
+  * event when using a keyboard to move around the page)
+  */
+
   handleClick = (event) => {
+
+    // Variables
+
+    let matchedDataList, 
+        matchedData,
+        textList,
+        text, 
+        source, 
+        src;
+
     const marker = event.target;
     const title = event.target.textContent;
     const matchedMarker = this.state.markers.find(m => title === m.options.title);
-    // console.log('data title:',this.state.data);
-    // console.log(marker.options.title);
-    let matchedDataList, textList, matchedData, text, source, src;
+
+    /* 
+    * Used different variables depending on whether the user 
+    * has clicked or keypressed a list view item or a marker in the map
+    */
+
     if (matchedMarker) {
       matchedDataList = this.state.data.find(i => i.title === title);
       textList = matchedDataList.text;
-      src = '<a href="https://en.wikipedia.org/wiki/' + title + '"target="_blank">Source</a>';
-      // console.log(matchedDataList);
+      src = '<a role="link" id="src-link" href="https://en.wikipedia.org/wiki/' + title + '"target="_blank">Source (Wikipedia)</a>';
     } else if (marker) {
       matchedData = this.state.data.find(item => item.title === marker.options.title);
       text = matchedData.text;
-      source = '<a href="https://en.wikipedia.org/wiki/' + marker.options.title + '"target="_blank">Source</a>';
-      // console.log(text);
+      source = '<a role="link" id="source-link" href="https://en.wikipedia.org/wiki/' + marker.options.title + '"target="_blank">Source (Wikipedia)</a>';
     }
-  
+
+    // Reset all markers to original styles
+
     if(this.state.prevMarker !== null) {
       if(marker && marker._leaflet_id === this.state.prevMarker._leaflet_id)
         return;
@@ -68,42 +96,60 @@ class App extends Component {
         {prevMarker: prevState.prevMarker._icon.style.height = '41px'}, 
         {prevMarker: prevState.prevMarker._icon.style.filter = ''}
       ])); 
-      // console.log('different markers');
     }
     
+    /* 
+    * Add clicked or selected marker to 'prevMarker' on state,
+    * change marker styles when selected and modify tab order 
+    * to allow users to interact with popup links and return to 
+    * last active element on list view or map
+    */
+
     if(matchedMarker) {
       this.setState({prevMarker: matchedMarker});
-      // console.log('matchedMarker data:', this.state.data);
-      matchedMarker.bindPopup(textList + '<br>' + src).openPopup();
+      
       matchedMarker._icon.style.width = '34px';
       matchedMarker._icon.style.height = '50px';
-      matchedMarker._icon.style.filter = 'hue-rotate(160deg)'; 
+      matchedMarker._icon.style.filter = 'hue-rotate(160deg)';
+      
+      matchedMarker.bindPopup(textList + '<br>' + src)
+      .on('popupclose', () => window.lastActive.focus())
+      .openPopup();
+      window.lastActive = document.activeElement;
+      document.getElementById('src-link').focus();
+
     } else if(marker) {
-      // console.log('handle click', this.state.data);
       this.setState({prevMarker: marker});
-      marker.bindPopup(text + '<br>' + source);
+
       marker._icon.style.width = '34px';
       marker._icon.style.height = '50px';
       marker._icon.style.filter = 'hue-rotate(160deg)';
-      // console.log('marker on map');
+      
+      marker.bindPopup(text + '<br>' + source)
+      .on('popupclose', () => window.lastActive.focus())
+      .openPopup();;
+      window.lastActive = document.activeElement;
+      document.getElementById('source-link').focus();
     }
-    
   }
 
+  // Create markers and load API data
+
   componentWillMount() {
-    const markers = this.state.allLocations.map(location => new L.marker([location.position.lat, location.position.lng], {title: location.title}));
+    const markers = this.state.allLocations.map(location => new L.marker(
+      [location.position.lat, location.position.lng], 
+      {title: location.title, alt: location.title}));
     this.setState({markers});
 
-    // console.log('will mount:', this.state.data);
     this.state.allLocations.forEach(loc => {
       let title = loc.title;
       this.getData(title);
-    })
+    });
   }
     
-  componentDidMount() {
-    // this.state.markers.forEach(marker => console.log(marker.options.title));     
-    // console.log('did mount data', this.state.data);   
+  // Initialize map, markers and popups
+
+  componentDidMount() {  
     const myMap = L.map('mapid', {
       center: [35.0517, 135.772057],
       zoom: 11
@@ -114,22 +160,26 @@ class App extends Component {
       id: 'mapbox.streets',
       accessToken: 'pk.eyJ1IjoianVkaXRocm4iLCJhIjoiY2pqZWZhaWh5Mm83ZjNxbW14YjYwY3BvdSJ9.dzLHt6jQRGlNH9jFAdhkbg'
     }).addTo(myMap);
-    // console.log('did mount:', this.state.data);
+
     this.state.markers.forEach(marker => {
-      // console.log('did mount:', this.state.data);
       marker.addTo(myMap);
       marker.bindPopup();
       marker.addEventListener('click', this.handleClick);
+      marker.addEventListener('keypress', this.handleClick);
     })  
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Neighborhood Map - Kyoto, Japan</h1>
+        <header className="App-header" role="banner">
+          <nav>
+            <h1 className="App-title">
+              <a href="/">Neighborhood Map - Kyoto, Japan</a>
+            </h1>
+          </nav>
         </header>
-        <div id="mapid"></div>
+        <div id="mapid" role="application" aria-label="neighborhood-map"></div>
         <ListView handleClick={this.handleClick} markers={this.state.markers} allLocations={this.state.allLocations}/>
       </div>
     );
--- src/ListView.js
+++ src/ListView.js
@@ -2,46 +2,19 @@ import React, { Component } from 'react';
 import escapeRegExp from 'escape-string-regexp'
 
 class ListView extends Component {
-    state = {
-        query: '',
-        previousMarker: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: '',
+            previousMarker: null
+        }
     }
-
+    
     updateQuery = (query) => {
         this.setState({query})
     }
 
-    // handleClick = (title) => {
-    //     console.log('The link was clicked.', title);
-
-    //     const matchedMarker = this.props.markers.find(m => title === m.options.title);
-        
-    //     if(this.state.previousMarker !== null) {
-    //         if(matchedMarker._leaflet_id === this.state.previousMarker._leaflet_id)
-    //             return;
-
-    //         this.setState(prevState => ([
-    //             {previousMarker: prevState.previousMarker._icon.style.width = '25px'},
-    //             {previousMarker: prevState.previousMarker._icon.style.height = '41px'}, 
-    //             {previousMarker: prevState.previousMarker._icon.style.filter = ''}
-    //         ])); 
-    //         console.log('different markers');
-    //         console.log('previousMarker width:', this.state.previousMarker._icon.style.width);
-    //     }
-
-    //     this.setState({previousMarker: matchedMarker});
-
-    //     matchedMarker.openPopup();
-    //     matchedMarker._icon.style.width = '34px';
-    //     matchedMarker._icon.style.height = '50px';
-    //     matchedMarker._icon.style.filter = 'hue-rotate(160deg)'; 
-    //     console.log(matchedMarker._leaflet_id);
-    //     console.log('previousMarker:', this.state.previousMarker);
-        
-    // }
-
     handleOnMouseOver = (title) => {
-        // console.log('mouseover:', title);
         title.style.fontSize = '20px';
     }
 
@@ -50,13 +23,13 @@ class ListView extends Component {
     }
         
     render() {
-        const {markers, allLocations, handleClick} = this.props;
+        const {allLocations, handleClick} = this.props;
         const {query} = this.state;
         let filteredPlaces;
-        // console.log(this.props.markers);
-        // this.props.markers.forEach(marker => console.log(marker.options.title));
+
         /* Filtering with RegExps as explained in the Udacity FEND course here: 
         https://github.com/udacity/reactnd-contacts-complete */
+
         if (query) {
             const match = new RegExp(escapeRegExp(query), 'i');
             filteredPlaces = allLocations.filter(place => match.test(place.title))
@@ -64,18 +37,27 @@ class ListView extends Component {
             filteredPlaces = allLocations;
         }
 
-        // console.log(filteredPlaces);
         return (
             <div className='list-view'>
                 <input 
-                    className='search-locations'
+                    role='search'
+                    aria-label='locations-filter'
+                    name='textfield'
+                    id='search-locations'
                     type='text' 
                     placeholder='Search location'
                     value={this.state.query}
                     onChange={event => this.updateQuery(event.target.value)} />
                 <ul className='locations-list'>
                     {filteredPlaces.map(item => (
-                        <li key={item.title} onMouseOver={event => this.handleOnMouseOver(event.target)} onMouseOut={event => this.handleOnMouseOut(event.target)} onClick={(event) => handleClick(event)} >{item.title}</li>
+                        <li tabIndex='0' 
+                            role='button' 
+                            key={item.title} 
+                            onMouseOver={event => this.handleOnMouseOver(event.target)} 
+                            onMouseOut={event => this.handleOnMouseOut(event.target)} 
+                            onClick={(event) => handleClick(event)} 
+                            onKeyPress={(event) => handleClick(event)}>{item.title}
+                        </li>
                     ))} 
                 </ul>
             </div>
